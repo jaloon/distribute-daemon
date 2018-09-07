7e77fc5c0db9ae3cce5955b5aeaab7b714923207
jaloon <jazurechen@163.com>
2018-09-07 17:38:45 +0800

    修复jar工具类中修改jar包中文件方法调用后报错，无法修改的bug

--- src/main/java/com/pltone/util/JarUtil.java
+++ src/main/java/com/pltone/util/JarUtil.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLEncoder;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
@@ -55,6 +58,20 @@ public class JarUtil {
         return clazz.getProtectionDomain().getCodeSource().getLocation().getPath();
     }
 
+    public static void main(String[] args) {
+        String path = System.getProperty("user.dir") + File.separator + "distribute-2.1.0.jar";
+        File file = new File(path);
+        System.out.println(file.exists());
+        String name = "test";
+        String content = "这是测试数据qqdwqdew";
+        try {
+            writeFileToJar(path, name, content.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     /**
      * 修改jar包里的文件或者添加文件
      *
@@ -64,15 +81,36 @@ public class JarUtil {
      * @throws IOException
      */
     public static void writeFileToJar(String jarPath, String fileName, byte[] fileData) throws IOException {
-        JarOutputStream jarOut = null;
         JarFile jarFile = null;
+        // 保持排列的顺序，所以用TreeMap，而不用HashMap
+        TreeMap<String, byte[]> tm = new TreeMap<>();
         try {
-            jarOut = new JarOutputStream(new FileOutputStream(jarPath));
+            // 将原Jar包里的所有内容读取到内存里
             jarFile = new JarFile(jarPath);
             Enumeration<JarEntry> enumeration = jarFile.entries();
-            boolean add = true;
             while (enumeration.hasMoreElements()) {
                 JarEntry jarEntry = enumeration.nextElement();
+                tm.put(jarEntry.getName(), inputStreamToByteArray(jarFile.getInputStream(jarEntry)));
+            }
+        } finally {
+            if (jarFile != null) {
+                try {
+                    jarFile.close();
+                } catch (IOException e) {
+                    // no exception expect
+                }
+            }
+        }
+        JarOutputStream jarOut = null;
+        try {
+            jarOut = new JarOutputStream(new FileOutputStream(jarPath));
+            Iterator<Map.Entry<String, byte[]>> iterator = tm.entrySet().iterator();
+            boolean add = true;
+            // 将TreeMap重新写到原jar里，如果TreeMap里已经有entryName文件那么覆盖，否则在最后添加
+            while (iterator.hasNext()) {
+                Map.Entry<String, byte[]> entry = iterator.next();
+                String name = entry.getKey();
+                JarEntry jarEntry = new JarEntry(name);
                 jarOut.putNextEntry(jarEntry);
                 if (jarEntry.getName().equals(fileName)) {
                     // 覆盖
@@ -80,7 +118,7 @@ public class JarUtil {
                     jarOut.write(fileData, 0, fileData.length);
                     continue;
                 }
-                byte[] bytes = inputStreamToByteArray(jarFile.getInputStream(jarEntry));
+                byte[] bytes = entry.getValue();
                 jarOut.write(bytes, 0, bytes.length);
             }
             if (add) {
@@ -89,17 +127,11 @@ public class JarUtil {
                 jarOut.putNextEntry(newJarEntry);
                 jarOut.write(fileData, 0, fileData.length);
             }
+            jarOut.finish();
         } finally {
             if (jarOut != null) {
                 try {
                     jarOut.close();
-                } catch (IOException e) {
-                    // no exception expect
-                }
-            }
-            if (jarFile != null) {
-                try {
-                    jarFile.close();
                 } catch (IOException e) {
                     // no exception expect
                 }
